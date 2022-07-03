e8163492f1d4f54da08e30dc0206bbd8705cb5e2
nioxsh <marcin.partyka@gsuite.us.edu.pl>
2022-07-03 20:18:14 +0200

    pppp

--- zdjecior.py
+++ zdjecior.py
@@ -1,8 +1,18 @@
 from PIL import Image
+from pynput.mouse import Listener
+mouse = Controller()
 
 img  = Image.open("test.jpg")
 #img.show()
 width = int(input('Podaj koordynat x:'))
 height = int(input('Podaj koordynat y:'))
+
 pixel_values = list(img.getdata())
-print(pixel_values[width*height])
+
+
+def on_click(x, y, button, pressed):
+    if pressed:
+        print(pixel_values[width*height])
+
+with Listener(on_click = on_click) as listener:
+    listener.join()
