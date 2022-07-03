964cc1482ff31935e84b6a0d395878174eee3fa7
nioxsh <marcin.partyka@gsuite.us.edu.pl>
2022-07-03 20:27:03 +0200

    dasda

--- zdjecior.py
+++ zdjecior.py
@@ -7,14 +7,14 @@ mouse = Controller()
 
 img  = Image.open("test.jpg")
 #img.show()
-width = int(input('Podaj koordynat x:'))
-height = int(input('Podaj koordynat y:'))
+#width = int(input('Podaj koordynat x:'))
+#height = int(input('Podaj koordynat y:'))
 
 pixel_values = list(img.getdata())
 
 def on_click(x, y, button, pressed):
     if pressed:
-        print(pixel_values[width*height])
+        print(mouse.position)
 
 with Listener(on_click = on_click) as listener:
     listener.join()
