aec5916149bdd939483339c35ee442dd21587eea
nabanaa <blazejekpp@gmail.com>
2022-07-03 20:31:11 +0200

    zmiany

--- zdjecior.py
+++ zdjecior.py
@@ -3,23 +3,16 @@ from pynput.mouse import Listener
 from pynput.mouse import Controller
 mouse = Controller()
 
-
-10
 img  = Image.open("test.jpg")
 pixel_values = list(img.getdata())
 
 def on_click(x, y, button, pressed):
+    width, height = img.size
     if pressed:
-        print(pixel_values[width*height]) #co to kurwa jest
+        print(pixel_values[width*y+x]) #co to kurwa jest
 
 with Listener(on_click = on_click) as listener:
     listener.join()
-# width, height = img.size
 
 
 
-
-# x = int(input('Podaj koordynat x:'))
-# y = int(input('Podaj koordynat y:'))
-# pixel_values = list(img.getdata())
-# print(pixel_values[width*y+x])
